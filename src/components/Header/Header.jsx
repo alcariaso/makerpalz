25966837ba22b6b12bbf2327f5e79135fadcedcc
Aldee <aldee.cariaso@gmail.com>
2024-03-14 23:55:04 -0600

    created header style and initial set-up. (for revision)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,23 @@
 import react from "react";
+import logo from "../../assets/logo/SJ_LOGO-1.png"
 import '../Header/Header.scss';
 
 
 const Header = () => {
 
     return (
-        <></>
+        <header className="header">
+            <nav className="header__nav">
+                <div className="navLogo">
+                    <img className="logo" src={logo} alt="makerpalz"/>
+                </div>
+                <ul className="navList">
+                    <li className="list">About</li>
+                    <li className="list">Contact Us</li>
+                </ul>
+            </nav>
+
+        </header>
     )
 }
 
